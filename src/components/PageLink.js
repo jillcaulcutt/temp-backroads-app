4049541446819803f0ee08414ae2d98abdaae682
Jillian Caulcutt <jcaulcutt@jestais.com>
2026-06-22 08:38:48 -0400

    first commit

---
+++ src/components/PageLink.js
@@ -0,0 +1,8 @@
+const PageLink = ({ link, itemClass }) => {
+  return (
+    <li>
+        <a href={link.href} className={itemClass} rel="noreferrer"> {link.text} </a>
+    </li>
+  )
+}
+export default PageLink
